6270720a6bea9fbc73b59ea279dcd4f700444378
discordstars <19372006+discordstars@users.noreply.github.com>
2020-07-15 00:52:24 +0100

    Redesign v2!

---
+++ js/copy.js
@@ -0,0 +1,11 @@
+$('.copy').click(function() {
+    copyToClipboard(this);
+});
+
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
---
+++ js/other.js
@@ -0,0 +1,9 @@
+//$(window).scroll(function () {
+//    if ($(window).scrollTop() > 10) {
+//        $('#logo').attr('src', 'img/logo_nom.png');
+//        $('nav').addClass('shadow bg-light');
+//    } else {
+//        $('#logo').attr('src', 'img/logo_nom_blanc_1.png');
+//        $('nav').removeClass('shadow bg-light');
+//    }
+//})
