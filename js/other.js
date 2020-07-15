1bb904dc5fa9ba5711779dc015437e5f0ce0bcca
discordstars <19372006+discordstars@users.noreply.github.com>
2020-07-15 18:03:38 +0100

    Remove unneeded files

--- js/other.js
+++
@@ -1,9 +0,0 @@
-//$(window).scroll(function () {
-//    if ($(window).scrollTop() > 10) {
-//        $('#logo').attr('src', 'img/logo_nom.png');
-//        $('nav').addClass('shadow bg-light');
-//    } else {
-//        $('#logo').attr('src', 'img/logo_nom_blanc_1.png');
-//        $('nav').removeClass('shadow bg-light');
-//    }
-//})
